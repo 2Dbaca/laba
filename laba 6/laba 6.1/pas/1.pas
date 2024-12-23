93933fbe6e1f2f86363aa5c4b05b979ac817148d
2Dbaca <kirillgon.kirill1@gmail.com>
2024-12-23 07:30:52 +0700

    Add files via upload

---
+++ laba 6/laba 6.1/pas/1.pas
@@ -0,0 +1,17 @@
+﻿function f(a, b: integer): integer;
+var
+  i: integer;
+begin
+  if a <= b then
+  begin
+    for i := a to b do
+    begin
+      WriteLn(i);
+    end;
+  end;
+  result := 0;
+end;
+
+begin
+  f(1, 10);
+end.
---
+++ laba 6/laba 6.1/pas/2.pas
@@ -0,0 +1,22 @@
+﻿function f(a, b: integer): integer;
+var
+  i: integer;
+  
+begin
+  if a <= b then
+  begin
+    for i := a to b do
+      WriteLn(i);
+  end
+  else
+  begin
+    for i := a downto b do
+      WriteLn(i);
+  end;
+
+  Result := 0;
+end;
+
+begin
+  f(5, -5);
+end.
