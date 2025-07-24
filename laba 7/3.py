821cc41cc9715e23368d01e25422ffdf56d41f10
2Dbaca <kirillgon.kirill1@gmail.com>
2025-07-24 21:39:45 +0700

    Delete laba 7 directory

--- laba 7/3.py
+++
@@ -1,7 +0,0 @@
-total = 0.0
-with open("f.txt","r") as file:
-    for line in file:
-
-        total += float(line.strip())**2
-print(f"Сумма квадратов компонента: {total}")
-file.close()
--- laba 7/5.py
+++
@@ -1,6 +0,0 @@
-
-with open("f.txt","r") as file:
-    for line in file:
-        last = line[-1].strip()
-print(f"Последний компонента: {line}")
-file.close()
